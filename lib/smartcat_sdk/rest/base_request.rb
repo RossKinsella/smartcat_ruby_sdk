46f1e59a11a9c0ee6519770226f32b8bb209569e
Ross Kinsella <rosskinsella0@gmail.com>
2019-04-11 13:49:15 +0100

    Update base_request.rb

--- lib/smartcat_sdk/rest/base_request.rb
+++ lib/smartcat_sdk/rest/base_request.rb
@@ -73,7 +73,7 @@ module SmartcatSDK
           response = @http.request request
           @last_response = response
           raise SmartcatSDK::REST::ServerError if response.is_a?(Net::HTTPServerError)
-          raise SmartcatSDK::REST::ServerError if response.status == 204
+          raise SmartcatSDK::REST::ServerError if response.code == 204
         rescue StandardError => _error
           raise if request.class == Net::HTTP::Post
           raise if retries_left <= 0
